7ac72bff5daa3707f0544a928986bdaed68f42b6
Duo Li <devel@ywemay.com>
2025-04-01 15:38:19 +0800

    Highlighted current page button

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { useLocation } from 'react-router-dom';
 import { Routes, Route, Link } from 'react-router-dom';
 import Democracy from './Democracy';
 import Passport from './Passport';
 
 function App() {
+  const location = useLocation();
   return (
     <div className="App">
       <header className="App-header">
-        <Link to="/democracy">Democracy</Link>
-        <Link to="/passport">Passport</Link>
+        {/* Conditionally apply the active class based on the current route */}
+        <Link to="/democracy" className={location.pathname === '/democracy' ? 'active' : ''}>Democracy</Link>
+        <Link to="/passport" className={location.pathname === '/passport' ? 'active' : ''}>Passport</Link>
         <Routes>
           <Route path="/democracy" element={<Democracy />} />
           <Route path="/passport" element={<Passport />} />
