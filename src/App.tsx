15a7c47367d85e94fd340f08324903a453abe03a
Duo Li <devel@ywemay.com>
2025-04-01 17:55:24 +0800

    Better text

--- src/App.tsx
+++ src/App.tsx
@@ -10,25 +10,29 @@ function App() {
   const location = useLocation();
   return (
     <div className="App">
-      <header className="App-header">
-        <Link to="/" className={location.pathname === '/' ? 'active' : ''}>Start</Link>
-        <Link to="/democracy" className={location.pathname === '/democracy' ? 'active' : ''}>Emergent Democracy</Link>
-        <Link to="/passport" className={location.pathname === '/passport' ? 'active' : ''}>Human Passport</Link>
-        <Routes>
-          <Route path="/democracy" element={<Democracy />} />
-          <Route path="/passport" element={<Passport />} />
-        </Routes>
-      </header>
-      <div className='page'>
-        <p>
-          This site reflects a vision on how the post scarcity economy may be achieved by humanity.
-          </p>
-        <p>
-          Here, you can find explanation of such concepts as Emergent democracy, Auto management of a system, Decentralized power, Post scarcity, Passport of human of planet earth.
-          </p>
-        <p>
-          The site intends to provide sufficient tools for any human from planet earth to start participating in Emerging democracy. And thus facilitating faster transition of humanity to post scarcity economy.
-          </p>
+      <div className="App-container">
+        <nav className="App-sidebar">
+          <Link to="/" className={location.pathname === '/' ? 'active' : ''}>Start</Link>
+          <Link to="/democracy" className={location.pathname === '/democracy' ? 'active' : ''}>Emergent Democracy</Link>
+          <Link to="/passport" className={location.pathname === '/passport' ? 'active' : ''}>Passport of Human of Planet Earth</Link>
+        </nav>
+        <main className="App-content">
+          <Routes>
+            <Route path="/democracy" element={<Democracy />} />
+            <Route path="/passport" element={<Passport />} />
+          </Routes>
+          <div className='page'>
+            <p>
+              This site reflects a vision on how the post scarcity economy may be achieved by humanity.
+            </p>
+            <p>
+              Here, you can find explanation of such concepts as Emergent democracy, Auto management of a system, Decentralized power, Post scarcity, Passport of human of planet earth.
+            </p>
+            <p>
+              The site intends to provide sufficient tools for any human from planet earth to start participating in Emerging democracy. And thus facilitating faster transition of humanity to post scarcity economy.
+            </p>
+          </div>
+        </main>
       </div>
     </div>
   );
